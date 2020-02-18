ab7393ad7656db4e5216a1cca9dad5a02b2cdc58
carlozdaniel <a20160383@gmail.com>
2020-02-17 23:52:14 -0600

    Users can create, read, update and delete (CRUD) lists of TODOs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,35 @@
 class ArticlesController < ApplicationController
 
- def show
-   
- end 
-def new
-  @article = Article.new
-end
-def create
-  @article = Article.create(title: params[:article][:title],content: params[:article][:content])
-  render json: @article
-end
+  def show
+    @article = Article.find(params[:id])
+  end 
+
+  def edit
+    @article = Article.find(params[:id])
+    puts "\n\n\n #{@article.persisted?} \n\n\n"
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title],content: params[:article][:content])
+    
+    redirect_to @article
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(title: params[:article][:title],content: params[:article][:content])
+    render json: @article
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy 
+    redirect_to root_path
+  end
+
 
 end
